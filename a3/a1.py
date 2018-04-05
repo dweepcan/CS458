ba9d582838bb4778b8947f53186a0837eabf48ee
CS458_W18_1181 <dm2shah@ugster20.student.cs>
2018-04-05 13:36:59 -0400

    Change code for a1.py

--- a3/a1.py
+++ a3/a1.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url="https://whoomp.cs.uwaterloo.ca/458a3/api/plain/send"
+url = "https://whoomp.cs.uwaterloo.ca/458a3/api/plain/send"
 payload = {
   "api_token": "18b02d363e372bd22c9c8e8dfe823bf6323c656eb5ca74dc406d1a221256d726",
   "to": "nessie",
@@ -13,14 +13,13 @@ headers = {
 }
 response = requests.post(url, data=json.dumps(payload), headers=headers)
 
-print(response.text)
-print(response.status_code, response.reason)
+print(response.text, response.status_code)
 
-url="https://whoomp.cs.uwaterloo.ca/458a3/api/plain/inbox"
+url = "https://whoomp.cs.uwaterloo.ca/458a3/api/plain/inbox"
 payload = {
   "api_token": "18b02d363e372bd22c9c8e8dfe823bf6323c656eb5ca74dc406d1a221256d726",
 }
 response = requests.post(url, data=json.dumps(payload), headers=headers)
 
-print(response.text)
-print(response.status_code, response.reason)
+print(response.text, response.status_code)
+
