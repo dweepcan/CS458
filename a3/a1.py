71e3c602643900a8d5bed78d88f54cf2c11dd4fd
dm2shah <dweepcan1@msn.com>
2018-04-05 12:37:43 -0400

    Add programming part 1

---
+++ a3/a1.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+url="https://whoomp.cs.uwaterloo.ca/458a3/api/plain/send"
+payload = {
+  "api_token": "18b02d363e372bd22c9c8e8dfe823bf6323c656eb5ca74dc406d1a221256d726",
+  "to": "nessie",
+  "message": "SGVsbG8sIHdvcmxkIQ=="
+}
+headers = {
+  "content-type": "application/json",
+  "accept": "application/json"
+}
+response = requests.post(url, data=json.dumps(payload), headers=headers)
+
+print(response.text)
+print(response.status_code, response.reason)
+
+url="https://whoomp.cs.uwaterloo.ca/458a3/api/plain/inbox"
+payload = {
+  "api_token": "18b02d363e372bd22c9c8e8dfe823bf6323c656eb5ca74dc406d1a221256d726",
+}
+response = requests.post(url, data=json.dumps(payload), headers=headers)
+
+print(response.text)
+print(response.status_code, response.reason)
